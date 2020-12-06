d8016251660cedca7ebd459da373355d89f714e1
akif <akiftasci@yahoo.com>
2020-12-06 10:45:24 +0300

    added 4

---
+++ src/Assignments/Assignment4.java
@@ -0,0 +1,27 @@
+package Assignments;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment4 {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\MONSTER\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://the-internet.herokuapp.com/");
+		driver.findElement(By.linkText("Multiple Windows")).click();
+		driver.findElement(By.xpath("//a[contains(@target, '_blank')]")).click();
+		Set<String> abc = driver.getWindowHandles();
+		Iterator<String> it = abc.iterator();
+		String parentWindow = it.next();
+		driver.switchTo().window(it.next());
+		System.out.println(driver.findElement(By.xpath("//div/h3")).getText());
+		driver.switchTo().window(parentWindow);
+		System.out.println(driver.findElement(By.xpath("//div[@id='content']/div/h3")).getText());
+	}
+
+}
